bc4597e7b14c58eb28cd09afa7bbc869027eb6b5
Branko Gagrica <bgagrica8919ri@raf.rs>
2022-02-06 15:00:25 +0100

    lst

--- appCrud.js
+++ appCrud.js
@@ -16,7 +16,7 @@ const appCrud = express()
 const server = http.createServer(appCrud);
 const io = new Server(server, {
     cors: {
-        origin: 'https://branko-app.herokuapp.com',
+        origin: 'https://gagrica-app.herokuapp.com/',
         
         methods: ['GET', 'POST'],
         credentials: true
